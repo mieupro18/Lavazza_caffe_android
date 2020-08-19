d88c9419f5227359bfd2d58e6faf3636bcd5fedc
Jaskar <jaskarj@mieupro.com>
2020-08-19 15:24:46 +0530

    update

--- src/dispenseScreen.js
+++ src/dispenseScreen.js
@@ -659,6 +659,7 @@ const styles = StyleSheet.create({
     fontSize: responsiveScreenFontSize(1.3),
   },
   orderStatusTextStyle: {
+    marginTop : '1%',
     color: '#100A45',
     fontSize: responsiveScreenFontSize(1.5),
   },
