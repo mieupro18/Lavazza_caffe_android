57abb7386aedaabb17e595c5fcc8369627457ed9
Jaskar <jaskarj@mieupro.com>
2020-09-10 18:46:29 +0530

    update

--- src/dispenseScreen.js
+++ src/dispenseScreen.js
@@ -183,7 +183,7 @@ export default class DispenseScreen extends Component {
             machineId: this.state.machineId,
             machineName: this.state.machineName,
           },
-          signal: getTimeoutSignal(5000).signal,
+          signal: (await getTimeoutSignal(5000)).signal,
         },
       )
         .then(response => response.json())
@@ -326,7 +326,7 @@ export default class DispenseScreen extends Component {
           machineId: this.state.machineId,
           machineName: this.state.machineName,
         },
-        signal: getTimeoutSignal(5000).signal,
+        signal:(await getTimeoutSignal(5000)).signal,
       },
     )
       .then(response => response.json())
@@ -388,7 +388,7 @@ export default class DispenseScreen extends Component {
           machineId: this.state.machineId,
           machineName: this.state.machineName,
         },
-        signal: getTimeoutSignal(10000).signal,
+        signal: (await getTimeoutSignal(10000)).signal,
       },
     )
       .then(response => response.json())
